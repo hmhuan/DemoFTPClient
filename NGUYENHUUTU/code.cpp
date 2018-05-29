f4c3fe4e6503a6e9ffef50335169982599532280
nguyenhuutu <huutu1009@gmail.com>
2018-05-29 21:10:51 +0700

    Commit: đã thêm đoạn code tính thời gian vào hàm get và put. Notes: kiểm tra lại hai hàm mget và mput

--- NGUYENHUUTU/code.cpp
+++ NGUYENHUUTU/code.cpp
@@ -198,3 +198,184 @@ bool activeMode(SOCKET soc, SOCKET &dsoc, char cmd[])
 
 	return true;
 }
+/*them code tinh thoi gian*/
+/*
+- Cần kiểm tra lại hàm mget nha: trong hàm mget có đoạn code xóa "mget" trong cmd trong khi bên trong file ftp_client.cpp đã xóa rồi
+- Kiểm tra lại hàm mput: chạy không đúng
+*/
+void get(SOCKET soc, string cmd, bool modePasv)
+{
+	
+	char buf[1024]; //??2048 4096
+	int Bytes;
+	SOCKET dataSocket;
+	ofstream fOut;
+	string temp, rm, lc;
+	clock_t clockStart, clockEnd;
+	unsigned long byteSum = 0;
+
+	int timeout = 0;
+	setsockopt(soc, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(int));
+
+	if (cmd == "")
+	{
+		std::cout << "Remote file: ";
+		getline(cin, rm);
+		std::cout << "Local file: ";
+		getline(cin, lc); //ten file up len server
+	}
+	else
+	{
+		rm = getFileName(cmd);
+		lc = getFileName(cmd);
+	}
+	if (lc == "")
+	{
+		for (int i = rm.length(); i >= 0; i--)
+		{
+			if (rm[i] == '\\' || rm[i] == '/')
+				break;
+			lc = rm[i] + lc;
+		}
+	}
+
+
+	fOut.open(lc, ios::out | ios::binary);
+	if (fOut.is_open())
+	{
+		bool check;
+		if (modePasv)
+			check = passiveMode(soc, dataSocket);
+		else
+			check = activeMode(soc, dataSocket);
+
+		memset(buf, 0, sizeof buf);
+		sprintf(buf, "RETR %s\r\n", rm.c_str());
+		Bytes = send(soc, buf, strlen(buf), 0);
+
+		memset(buf, 0, sizeof buf);
+		Bytes = recv(soc, buf, sizeof buf, 0);
+		printf("%s", buf);
+		sscanf(buf, "%d", &Bytes);
+		if (check && Bytes != 550 && Bytes != 501)
+		{
+			
+			if (!modePasv)
+				dataSocket = accept(dataSocket, NULL, NULL);
+			
+			clockStart = clock();
+			while (1)
+			{
+				memset(buf, 0, sizeof buf);
+				Bytes = recv(dataSocket, buf, sizeof buf, 0);
+				byteSum += Bytes;
+				fOut.write(buf, Bytes);
+				if (Bytes != sizeof buf)
+					break;
+			}
+			clockEnd = clock();	
+		}
+		fOut.close();
+		closesocket(dataSocket);
+		//connSocket trả về thông báo gửi thành công/ thất bại 
+		memset(buf, 0, sizeof buf);
+		Bytes = recv(soc, buf, sizeof buf, 0);
+		printf("%s", buf);
+		double times = (clockEnd - clockStart) * 1.0 / CLOCKS_PER_SEC;
+		printf("%lu bytes received in %0.3lfseconds %0.3lfKbytes/sec.\n", byteSum, times, (byteSum*1.0 / 1024) / times);
+	}
+	else
+	{
+		cout << "> R: No such process\n";
+	}
+}
+void put(SOCKET soc, string cmd, bool modePasv)
+{
+	char buf[4096]; //co the tang buffer them??
+	string temp, lc, rm;
+	int Bytes;
+	SOCKET dataSocket;
+	ifstream fIn;
+
+	int timeout = 0, flag;
+	setsockopt(soc, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(int));
+	chuanHoa(cmd);
+	////TH1: put <lcfile> [<rmfile>]
+	if (cmd == "")
+	{
+		printf("Remote file: ");
+		getline(cin, lc);
+		printf("Local file: ");
+		getline(cin, rm); //ten file up len server
+	}
+	else
+	{
+		lc = getFileName(cmd);
+		rm = getFileName(cmd);
+	}
+	if (rm == "")
+	{
+		for (int i = lc.length(); i >= 0; i--)
+		{
+			if (lc[i] == '\\' || lc[i] == '/')
+				break;
+			rm = lc[i] + rm;
+		}
+	}
+
+	fIn.open(lc, ios::in | ios::binary);
+	if (fIn.is_open())
+	{
+		bool check;
+		if (modePasv)
+			check = passiveMode(soc, dataSocket);
+		else
+			check = activeMode(soc, dataSocket);
+
+		memset(buf, 0, sizeof buf);
+		sprintf(buf, "STOR %s\r\n", rm.c_str());
+		Bytes = send(soc, buf, strlen(buf), 0);
+		memset(buf, 0, sizeof buf);
+		Bytes = recv(soc, buf, sizeof buf, 0);
+		printf("%s", buf);
+
+		sscanf(buf, "%d", &Bytes);
+		if (check && Bytes != 550 && Bytes != 501)
+		{
+			if (!modePasv)
+				dataSocket = accept(dataSocket, NULL, NULL);
+
+			fIn.seekg(0, fIn.end);
+			unsigned byteSum = fIn.tellg();
+			long long int Size = (fIn.tellg()) % (sizeof buf);
+			fIn.seekg(0, fIn.beg);
+
+			clock_t clockStart, clockEnd;
+			clockStart = clock();
+			while (!fIn.eof())
+			{
+				memset(buf, 0, sizeof buf);
+				fIn.read(buf, sizeof buf);
+				if (fIn.eof())
+				{
+					send(dataSocket, buf, Size, 0);
+					break;
+				}
+				send(dataSocket, buf, sizeof buf, 0);
+			}
+			clockEnd = clock();
+			double times = (clockEnd - clockStart)*1.0 / CLOCKS_PER_SEC;
+			closesocket(dataSocket);
+			//connSocket trả về cmd 
+			memset(buf, 0, sizeof buf);
+			Bytes = recv(soc, buf, sizeof buf, 0);
+			printf("%s", buf);
+			fIn.close();
+			printf("ftp: %lu bytes sent in %0.3lfSeconds %0.3lfKbytes/sec\n", byteSum, times, byteSum*1.0 / 1024 / times);
+		}
+	}
+	else
+	{
+		cout << lc << " File not found\n";
+	}
+}
